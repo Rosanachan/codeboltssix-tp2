aac7b05d6fae0c7ce34b94bf534b89aca3d599bf
Rosana Herrera <neuyin.herrera.sun@gmail.com>
2025-10-17 12:38:10 -0300

    TP2 - proyecto Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Sidebar from './components/Sidebar'
+
+function App() {
+
+  return (
+    <> <h1>Codebolts Six</h1>
+    <Sidebar/>
+    </>
+  )
+}
+
+export default App
