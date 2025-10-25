dadb5fd320858cb74a7ed4eb277e93b28d2603e5
Erick Johan Romero Ninanya <masterfigh@gmail.com>
2025-10-25 01:17:06 -0300

    add footer and minor fix in home

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../index.css"; 
+
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <p className="footer-text">
+        <strong>Desarrollo de Sistemas Web (Front End) - Proyecto académico IFTS29</strong>
+      </p>
+      <p><strong>⚡ © 2025CodeBolts Six: Evolution</strong></p>
+    </footer>
+  );
+}
